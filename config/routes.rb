873c6f15d74f09f36e10a489f231b58472e4ec89
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-01-22 12:10:35 +0530

    Added better user editing

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "investments#index"
+  root "companies#dashboard"
 end
