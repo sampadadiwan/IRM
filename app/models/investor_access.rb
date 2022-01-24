f38b93212dbb7fde989e2fe61e0ae1e577e2dc05
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-01-24 20:00:37 +0530

    Added better investor cap table access

--- app/models/investor_access.rb
+++ app/models/investor_access.rb
@@ -5,6 +5,11 @@ class InvestorAccess < ApplicationRecord
     has_one :investor_entity, through: :investor
     has_one :investee_entity, through: :investor
 
+    ALL = "All Investments"
+    SELF = "Self Investments"
+    SUMMARY = "Summary Of Investments"
+    VIEWS = [ALL, SELF, SUMMARY]
+
     after_create :send_notification
     def send_notification
         InvestorAccessMailer.with(investor_access:self).notify_access.deliver_later
