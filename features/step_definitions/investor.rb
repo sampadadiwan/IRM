1231984282e0ae5f59a8625c88799fffd3292337
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-02-23 17:07:53 +0530

    Fixed tests

--- features/step_definitions/investor.rb
+++ features/step_definitions/investor.rb
@@ -31,7 +31,7 @@ Then('an investor entity should be created') do
 end
 
 Then('an investor entity should not be created') do
-  Entity.where(name: @investor.investor_name).count.should == 0
+  Entity.where(name: @investor.investor_name).count.should == 1
 
   @investor_entity.name.include?(@investor_entity.name).should == true
   @investor.investor_entity_id.should == @investor_entity.id
