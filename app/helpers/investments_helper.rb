4508c27900146cda0f727d5b697cc42c6f8627bb
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-03-08 23:21:00 +0530

    Fixed numbers in investments post Money gem

--- app/helpers/investments_helper.rb
+++ app/helpers/investments_helper.rb
@@ -1,13 +1,11 @@
 module InvestmentsHelper
   def money_to_currency(amount, symbol)
-    Rails.logger.debug { "xxxxxxxxxxxxxxxxxx #{symbol}" }
     case symbol.strip
     when "INR"
       Money.new(amount, "INR").format(south_asian_number_formatting: true)
     when "SGD"
       Money.new(amount, "SGD").format
     when "USD"
-      Rails.logger.debug { "################ #{symbol}" }
       Money.new(amount, "USD").format
     else
       Money.new(amount, "INR").format(south_asian_number_formatting: true)
