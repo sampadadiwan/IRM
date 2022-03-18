574bba1feea53ba86abe41e7939f371f3e213426
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-03-18 18:37:31 +0530

    Fixed currency selector for Investor view

--- app/javascript/controllers/currency_controller.js
+++ app/javascript/controllers/currency_controller.js
@@ -10,10 +10,29 @@ export default class extends Controller {
     let units = $("#currency_units").val();
     console.log(`setCurrencyUnit called ${units}`);
     let form = $("#currency_units_form");
-    let actionUrl = window.location.href;
+    // Set the action of the form to the current url, so we can request the same data with new currency
+    let actionUrl = window.location.href.split("?")[0];
     console.log(actionUrl);
     form.attr('action', actionUrl);
-    let submit = $(event.target).closest("form").find("input[type='submit']");
+
+    let query = window.location.search.substring(1);
+    console.log(`query = ${query}`)
+    let vars = query.split('&');
+    console.log(`vars = ${vars}`);
+    
+    // We need to make sure all params in the URL are sent back
+    // So add them to the form
+    vars.forEach(keyVal => {
+      let keyValArr = keyVal.split('=');
+      if(keyValArr[0] != "units") {
+        form.append(`<input type='text' hidden="hidden" name='${keyValArr[0]}' value='${keyValArr[1]}' />`); 
+      }
+    });
+    
+    // Make the call
+    let submit = form.find("input[type='submit']");
     submit.click();
   }
+
+
 }
