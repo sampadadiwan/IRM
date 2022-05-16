e819a6f14385102a08e85f9e82ed9a398dbcdb62
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-05-16 07:51:49 +0530

    Added delete confirm

--- app/javascript/controllers/wizard_controller.js
+++ app/javascript/controllers/wizard_controller.js
@@ -1,20 +1,56 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
+    connect() {
 
-    // Setup the wizard js
-    $('#smartwizard').smartWizard({
-        theme: 'dots',
-    });
+        this.init();
+        
+        $(document).on('turbo:load', function () {
+            this.init();
+        });
 
-    // Hide/Show the search fields based on investor type
-    $("#search_user_group").toggle();
-    $("#investor_investor_type").on("change", function(){
-      console.log("investor_investor_type: change");
-      $("#search_entity_group").toggle();
-      $("#search_user_group").toggle();
-    });
+    }
 
-  }
+    init() {
+        var navListItems = $('div.setup-panel div a'),
+            allWells = $('.setup-content'),
+            allNextBtn = $('.nextBtn');
+
+        allWells.hide();
+
+        navListItems.click(function (e) {
+            e.preventDefault();
+            var $target = $($(this).attr('href')),
+                $item = $(this);
+
+            if (!$item.hasClass('disabled')) {
+                navListItems.removeClass('btn-primary').addClass('btn-default');
+                $item.addClass('btn-primary');
+                allWells.hide();
+                $target.show();
+                $target.find('input:eq(0)').focus();
+            }
+        });
+
+        allNextBtn.click(function () {
+            var curStep = $(this).closest(".setup-content"),
+                curStepBtn = curStep.attr("id"),
+                nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+                curInputs = curStep.find("input[type='text'],input[type='number'],input[type='date'],select,textarea"),
+                isValid = true;
+
+            $(".form-group").removeClass("field_with_errors");
+            for (var i = 0; i < curInputs.length; i++) {
+                if (!curInputs[i].validity.valid) {
+                    isValid = false;
+                    $(curInputs[i]).closest(".form-group").addClass("field_with_errors");
+                }
+            }
+
+            if (isValid)
+                nextStepWizard.removeAttr('disabled').trigger('click');
+        });
+
+        $('div.setup-panel div a.btn-primary').trigger('click');
+    }
 }
